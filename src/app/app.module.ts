f2115d77cb7c0eea0ab3a654b0cba9e354ee2ed5
John P <70922864+p134c0d3@users.noreply.github.com>
2023-11-02 21:08:15 -0500

    App Creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { BugsComponent } from './bugs/bugs.component';
+import { ClientsComponent } from './bugs/clients/clients.component';
+import { ClientComponent } from './bugs/clients/client/client.component';
+import { ProjectsComponent } from './bugs/projects/projects.component';
+import { ProjectComponent } from './bugs/projects/project/project.component';
+import { BugComponent } from './bugs/projects/project/bug/bug.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    BugsComponent,
+    ClientsComponent,
+    ClientComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    BugComponent,
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
